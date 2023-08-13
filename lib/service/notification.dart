635b219ff8b23f288326848292cdbcc546c3a0a9
app <app.managementacc@gmail.com>
2023-08-14 07:39:32 +0900

    update

--- lib/service/notification.dart
+++ lib/service/notification.dart
@@ -115,21 +115,21 @@ class AlarmNotification {
       enableLights: true,
       fullScreenIntent: true,
       ongoing: true,
-      ticker: 'ticker',
-      actions: <AndroidNotificationAction>[
-        AndroidNotificationAction(
-          'text_id_2',
-          'Action 2',
-          icon: DrawableResourceAndroidBitmap('food'),
-          inputs: <AndroidNotificationActionInput>[
-            AndroidNotificationActionInput(
-              choices: <String>['ABC', 'DEF'],
-              allowFreeFormInput: false,
-            ),
-          ],
-          contextual: true,
-        ),
-      ],
+      // ticker: 'ticker',
+      // actions: <AndroidNotificationAction>[
+      //   AndroidNotificationAction(
+      //     'text_id_2',
+      //     'Action 2',
+      //     icon: DrawableResourceAndroidBitmap('food'),
+      //     inputs: <AndroidNotificationActionInput>[
+      //       AndroidNotificationActionInput(
+      //         choices: <String>['ABC', 'DEF'],
+      //         allowFreeFormInput: false,
+      //       ),
+      //     ],
+      //     contextual: true,
+      //   ),
+      // ],
       // actions: <AndroidNotificationAction>[
       //   AndroidNotificationAction(
       //     'text_id',
