d1a7daa4f8e0bf9be16094d0e6a4c8bfd2c0516b
app <app.managementacc@gmail.com>
2023-08-14 10:41:15 +0900

    update

--- lib/service/notification.dart
+++ lib/service/notification.dart
@@ -120,7 +120,6 @@ class AlarmNotification {
         AndroidNotificationAction(
           'text_id_2',
           'Action 2',
-          icon: DrawableResourceAndroidBitmap('food'),
           inputs: <AndroidNotificationActionInput>[
             AndroidNotificationActionInput(
               choices: <String>['ABC', 'DEF'],
